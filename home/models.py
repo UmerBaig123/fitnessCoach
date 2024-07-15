67db83428bf2d2993b900643b24e0949b81272e4
umerbaig123 <=>
2024-07-15 01:01:14 -0700

    Does minor changes in models

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -12,8 +13,9 @@ class user_foods(models.Model):
     portion_unit = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class log(models.Model):
-    date = models.DateField()
+    date = models.DateField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class log_foods(models.Model):
@@ -26,11 +28,15 @@ class log_foods(models.Model):
     portion_unit = models.CharField(max_length=100)
     time_added = models.TimeField(auto_now_add=True)
     log = models.ForeignKey(log, on_delete=models.CASCADE)
-    def addFromObject(self, user_food):
-        self.name = user_food.name
-        self.calories = user_food.calories
-        self.fat = user_food.fat
-        self.protein = user_food.protein
-        self.carbs = user_food.carbs
-        self.portion = user_food.portion
-        self.portion_unit = user_food.portion_unit
+    def addFromObject(user_food,user):
+        name = user_food.name
+        calories = user_food.calories
+        fat = user_food.fat
+        protein = user_food.protein
+        carbs = user_food.carbs
+        portion = user_food.portion
+        portion_unit = user_food.portion_unit
+        log = log.objects.get(user=user,date=datetime.date.today())
+        log_food = log_foods( name=name, calories=calories, fat=fat, protein=protein, carbs=carbs, portion=portion, portion_unit=portion_unit, log=log)
+        log_food.save()
+    
